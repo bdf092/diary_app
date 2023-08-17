cdd3eddeb3ea70a5a0718a2d3290ae21c1759882
Barbara Fajardo <barbarafajardo92@gmail.com>
2023-08-17 16:15:43 +0100

    add DiaryEntries Model

---
+++ server/models/diary.js
@@ -0,0 +1,14 @@
+class DiaryEntries {
+
+    constructor(data) {
+          this.id = data.id
+          this.date = data.date
+          this.time = data.time
+          this.text = data.text
+          this.cateogry = data.category
+    
+        }
+      
+      }
+    
+    module.exports = DiaryEntries
